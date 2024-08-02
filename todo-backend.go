22465b153612a8369769767c1993537afedf22c7
weeeeestern <liz0824@naver.com>
2024-08-02 15:26:46 +0900

    Update to use enviroment variables
    Signed-off-by: weeeeestern <liz0824@naver.com>

--- todo-backend.go
+++ todo-backend.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -20,7 +22,13 @@ var db *gorm.DB
 var err error
 
 func init() {
-	dsn := "user:password@tcp(localhost:3306)/todoapp?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"))
+
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
